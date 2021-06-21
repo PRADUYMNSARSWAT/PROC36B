488dd49b89d412cc547a63a00946712594f4bf77
PRADUYMNSARSWAT <77499690+PRADUYMNSARSWAT@users.noreply.github.com>
2021-06-21 13:51:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var hypnoticBall, database;
+var position;
+var gameState = 0 ;
+var playerCount;
+var form,game,player ;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+  background("white");
+  
+
+  
+}
